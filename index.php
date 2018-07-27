fe9a9b98e5f3bcde231571eea8097e96288ac7b4
Felipe Gomes Duarte <felipegdmito@gmail.com>
2018-07-27 04:57:26 +0000

    comentários terminados

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php 
     echo "Hello word";
     /*
+        git init
+        git add --all
+        git commit -m "obs sobre push"
         git remote add origin https://github.com/xFelipe/php-7.git
         git push -f origin master
     */
