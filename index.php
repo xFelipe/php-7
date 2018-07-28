4f0b902ddb61e881191cdb4109d018eddbad5b8b
Felipe Gomes Duarte <felipegdmito@gmail.com>
2018-07-28 03:21:55 +0000

    adicionando correção de conflitos

--- index.php
+++ index.php
@@ -6,5 +6,9 @@
         git commit -m "obs sobre push"
         git remote add origin https://github.com/xFelipe/php-7.git
         git push -f origin master
+        
+        Limpa edições e pega do repositório:
+        git stash save --keep-index
+        git pull origin master
     */
 ?>//
